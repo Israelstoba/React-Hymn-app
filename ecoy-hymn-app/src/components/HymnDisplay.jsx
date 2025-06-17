6fc27dea0391e0364f80e838039d844b7328e859
israelstoba <israelstoba@gmail.com>
2025-06-17 14:04:26 +0100

    minor commit

--- ecoy-hymn-app/src/components/HymnDisplay.jsx
+++ ecoy-hymn-app/src/components/HymnDisplay.jsx
@@ -1,7 +1,6 @@
 // src/pages/HymnDisplay.jsx
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { databases } from '../lib/appwrite';
 
