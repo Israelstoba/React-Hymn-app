a9bec48aceb8cbd11db6c6873c0b7d37cc2dd951
israelstoba <israelstoba@gmail.com>
2025-06-17 13:49:41 +0100

    fixing hymn display

--- ecoy-hymn-app/src/components/HymnDisplay.jsx
+++ ecoy-hymn-app/src/components/HymnDisplay.jsx
@@ -2,14 +2,13 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
-import { databases } from '../lib/appwrite';
 import Footer from '../components/Footer';
+import { databases } from '../lib/appwrite';
 
 function HymnDisplay() {
   const { id } = useParams();
   const [hymn, setHymn] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [activePart, setActivePart] = useState('verse');
 
   useEffect(() => {
     const fetchHymn = async () => {
@@ -32,38 +31,16 @@ function HymnDisplay() {
   if (loading) return <p style={{ padding: '20px' }}>Loading...</p>;
   if (!hymn) return <p style={{ padding: '20px' }}>Hymn not found.</p>;
 
-  const [versesPart, chorusPart] = hymn.lyrics.split(/Chorus:/i);
-  const hasChorus = Boolean(chorusPart);
-
   return (
-    <div className="hymn-display-con">
-      <h4 className="hymn-title">{hymn.title}</h4>
-      {hasChorus && (
-        <div className="hymn-tabs">
-          <button
-            className={activePart === 'verse' ? 'active' : ''}
-            onClick={() => setActivePart('verse')}
-          >
-            Verse
-          </button>
-          <button
-            className={activePart === 'chorus' ? 'active' : ''}
-            onClick={() => setActivePart('chorus')}
-          >
-            Chorus
-          </button>
+    <>
+      <div className="hymn-display-con">
+        <h2 className="hymn-title">{hymn.title}</h2>
+        <div className="hymn-lyrics-wrapper">
+          <pre className="hymn-lyrics">{hymn.lyrics}</pre>
         </div>
-      )}
-      <div className="hymn-text">
-        <pre>
-          {activePart === 'verse'
-            ? versesPart.trim()
-            : hasChorus
-            ? 'Chorus:\n' + chorusPart.trim()
-            : versesPart.trim()}
-        </pre>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
